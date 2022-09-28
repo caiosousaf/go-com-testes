b59f3b5a76054c2522117ecdf18328e989ac20ca
Caio Sousa <caiosousafernandesferreira@hotmail.com>
2022-09-28 09:10:37 -0300

    :test_tube: test: Criando novo teste Idioma Espanhol

--- ola_mundo/ola.go
+++ ola_mundo/ola.go
@@ -1,11 +1,31 @@
 package main
 
-import "fmt"
 
-func Ola(nome string) string {
-    return "Olá, " + nome
+func Ola(nome, idioma string) string {
+    const (
+        prefixoOlaPortugues = "Olá, "
+        prefixoOlaEspannhol = "Hola, "
+        prefixoOlaIngles = "Hello, "
+    )
+
+    if nome == "" {
+        switch idioma {
+        case "ingles":
+            nome = "world"
+        case "espanhol":
+            nome = "mundo"
+        case "portugues":
+            nome = "mundo"
+        }
+    }
+    switch idioma {
+    case "portugues":
+        return prefixoOlaPortugues + nome
+    case "espanhol":
+        return prefixoOlaEspannhol + nome
+    case "ingles":
+        return prefixoOlaIngles + nome
+    }
+    return nome + ",O idioma não foi encontrado"
 }
 
-func main() {
-    fmt.Println(Ola("Casa"))
-}
--- ola_mundo/ola_test.go
+++ ola_mundo/ola_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-    resultado := Ola("Caio")
-    esperado := "Olá, Caio"
 
-    if resultado != esperado {
-        t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-    }
+    t.Run("Teste-Idioma-BR", func (t *testing.T) {
+        resultado := Ola("Caio", "portugues")
+        esperado := "Olá, Caio"
+    
+        if resultado != esperado {
+            t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+        }
+
+    })
+
+    t.Run("Teste-Idioma-ES", func(t *testing.T) {
+        resultado := Ola("Bruno", "espanhol")
+        esperado := "Hola, Bruno"
+
+        if resultado != esperado {
+            t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+        }
+    })
+
+    
 }
