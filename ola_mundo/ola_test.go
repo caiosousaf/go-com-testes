3c04350d18e67676bc0b25c56b8afffc580d04f4
Caio Sousa <caiosousafernandesferreira@hotmail.com>
2022-09-28 09:28:12 -0300

    :test_tube: test: Criando novo teste com funcao auxiliar para evitar linhas desnecessárias

--- ola_mundo/ola_test.go
+++ ola_mundo/ola_test.go
@@ -3,14 +3,18 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
+    verificaMensagem := func(t *testing.T, resultado, esperado string)  {
+        t.Helper()
+        if resultado != esperado {
+            t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+        }
+    }
 
     t.Run("Teste-Idioma-BR", func (t *testing.T) {
         resultado := Ola("Caio", "portugues")
         esperado := "Olá, Caio"
     
-        if resultado != esperado {
-            t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-        }
+        verificaMensagem(t, resultado, esperado)
 
     })
 
@@ -18,44 +22,34 @@ func TestOla(t *testing.T) {
         resultado := Ola("Bruno", "espanhol")
         esperado := "Hola, Bruno"
 
-        if resultado != esperado {
-            t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-        }
+        verificaMensagem(t, resultado, esperado)
     })
 
     t.Run("RetornarMundoQuandoNomeVazio", func(t *testing.T) {
         resultado := Ola("", "portugues")
         esperado := "Olá, mundo"
 
-        if resultado != esperado {
-            t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-        }
+        verificaMensagem(t, resultado, esperado)
     })
 
     t.Run("Teste-Idioma-EN-Nome-Vazio", func(t *testing.T) {
         resultado := Ola("", "ingles")
         esperado := "Hello, world"
 
-        if resultado != esperado {
-            t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-        }
+        verificaMensagem(t, resultado, esperado)
     })
 
     t.Run("Teste-Idioma-ES-Nome-Vazio", func(t *testing.T) {
         resultado := Ola("", "espanhol")
         esperado := "Hola, mundo"
 
-        if resultado != esperado {
-            t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-        }
+        verificaMensagem(t, resultado, esperado)
     })
 
     t.Run("Teste-Idioma-Nao-Encontrado", func(t *testing.T) {
         resultado := Ola("Caio", "")
         esperado := "Caio,O idioma não foi encontrado"
 
-        if resultado != esperado {
-            t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-        }
+        verificaMensagem(t, resultado, esperado)
     })
 }
