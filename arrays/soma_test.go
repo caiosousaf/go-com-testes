c54366e38245c821c02f93bd689071f0728ab4ff
Caio Sousa <caiosousafernandesferreira@hotmail.com>
2022-09-29 10:59:55 -0300

    :test_tube: test: Criando novo teste Arrays fatiados e iteracao de arrays

--- arrays/soma_test.go
+++ arrays/soma_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSoma(t *testing.T) {
 	numeros := []int{1, 2, 3, 4, 5}
@@ -29,4 +32,37 @@ func TestSoma(t *testing.T) {
             t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numeros)
         }
     })
+}
+
+func TestSomaTudo(t *testing.T) {
+
+    recebido := SomaTudo([]int{1,2}, []int{0,9})
+    esperado := []int{3, 9}
+
+    if !reflect.DeepEqual(recebido, esperado) {
+        t.Errorf("recebido %v esperado %v", recebido, esperado)
+    }
+}
+
+func TestSomaTodoOResto(t *testing.T) {
+	verificaSomas := func(t *testing.T, resultado, esperado []int) {
+        t.Helper()
+        if !reflect.DeepEqual(resultado, esperado) {
+            t.Errorf("resultado %v, esperado %v", resultado, esperado)
+        }
+    }
+
+	t.Run("Faz-Soma-Slices", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{1,2}, []int{0,9})
+		esperado := []int{2, 9}
+	
+		verificaSomas(t,resultado, esperado)
+	})
+
+	t.Run("soma-slices-vazio", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{}, []int{3, 4, 5})
+        esperado := []int{0, 9}
+
+        verificaSomas(t, resultado, esperado)
+	})
 }
