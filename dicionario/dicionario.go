550a2710eb5270798754b5690ec50906d8a306eb
Caio Sousa <caiosousafernandesferreira@hotmail.com>
2022-10-18 08:44:10 -0300

    :test_tube: test: Created new test. Dictionary update success and error

--- dicionario/dicionario.go
+++ dicionario/dicionario.go
@@ -1,19 +1,18 @@
 package dicionario
 
-
 type Dicionario map[string]string
 
 type ErrDicionario string
 
 func (e ErrDicionario) Error() string {
 	return string(e)
-} 
+}
 
 var (
-	ErrNaoEncontrado = ErrDicionario("não foi possivel encontrar a palavra que voce procura")
+	ErrNaoEncontrado    = ErrDicionario("não foi possivel encontrar a palavra que voce procura")
 	ErrPalavraExistente = ErrDicionario("não é possivel adicionar a palavra pois ela já existe")
+	ErrPalavraInexistente = ErrDicionario("não foi possível atualizar a palavra pois ela não existe")
 )
-
 
 func (d Dicionario) Busca(palavra string) (string, error) {
 	definicao, existe := d[palavra]
@@ -21,16 +20,30 @@ func (d Dicionario) Busca(palavra string) (string, error) {
 		return "", ErrNaoEncontrado
 	}
 
-    return definicao, nil
+	return definicao, nil
 }
 
 func (d Dicionario) Adiciona(palavra, definicao string) error {
 	_, err := d.Busca(palavra)
+	switch err {
+	case ErrNaoEncontrado:
+		d[palavra] = definicao
+	case nil:
+		return ErrPalavraExistente
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dicionario) Atualiza(palavra, definicao string) error{
+	_, err := d.Busca(palavra)
     switch err {
     case ErrNaoEncontrado:
-        d[palavra] = definicao
+        return ErrPalavraInexistente
     case nil:
-        return ErrPalavraExistente
+        d[palavra] = definicao
     default:
         return err
 
--- dicionario/dicionario_test.go
+++ dicionario/dicionario_test.go
@@ -5,15 +5,15 @@ import "testing"
 func TestBusca(t *testing.T) {
 	dicionario := Dicionario{"teste": "isso é apenas um teste", "sla": "Opa"}
 	t.Run("Palavra conhecida", func(t *testing.T) {
-	
+
 		resultado, _ := dicionario.Busca("teste")
 		esperado := "isso é apenas um teste"
-	
+
 		comparaStrings(t, resultado, esperado)
 	})
 
 	t.Run("palavra desconhecida", func(t *testing.T) {
-		 _, resultado := dicionario.Busca("desconhecida")
+		_, resultado := dicionario.Busca("desconhecida")
 
 		comparaErro(t, resultado, ErrNaoEncontrado)
 	})
@@ -26,46 +26,69 @@ func TestAdiciona(t *testing.T) {
 		palavra := "teste"
 		definicao := "isso é apenas um teste"
 		dicionario.Adiciona(palavra, definicao)
-	
+
 		comparaDefinicao(t, dicionario, palavra, definicao)
 	})
 
 	t.Run("palavra existente", func(t *testing.T) {
 		palavra := "teste"
-        definicao := "isso é apenas um teste"
-        dicionario := Dicionario{palavra: definicao}
-        err := dicionario.Adiciona(palavra, "teste novo")
+		definicao := "isso é apenas um teste"
+		dicionario := Dicionario{palavra: definicao}
+		err := dicionario.Adiciona(palavra, "teste novo")
 
-        comparaErro(t, err, ErrPalavraExistente)
-        comparaDefinicao(t, dicionario, palavra, definicao)
+		comparaErro(t, err, ErrPalavraExistente)
+		comparaDefinicao(t, dicionario, palavra, definicao)
 	})
 }
 
 func comparaDefinicao(t *testing.T, dicionario Dicionario, palavra, definicao string) {
-    t.Helper()
+	t.Helper()
 
-    resultado, err := dicionario.Busca(palavra)
-    if err != nil {
-        t.Fatal("deveria ter encontrado palavra adicionada:", err)
-    }
+	resultado, err := dicionario.Busca(palavra)
+	if err != nil {
+		t.Fatal("deveria ter encontrado palavra adicionada:", err)
+	}
 
-    if definicao != resultado {
-        t.Errorf("resultado '%s',  esperado '%s'", resultado, definicao)
-    }
+	if definicao != resultado {
+		t.Errorf("resultado '%s',  esperado '%s'", resultado, definicao)
+	}
 }
 
 func comparaErro(t *testing.T, resultado, esperado error) {
-    t.Helper()
+	t.Helper()
 
-    if resultado != esperado {
-        t.Errorf("resultado erro '%s', esperado '%s'", resultado, esperado)
-    }
+	if resultado != esperado {
+		t.Errorf("resultado erro '%s', esperado '%s'", resultado, esperado)
+	}
 }
 
 func comparaStrings(t *testing.T, resultado, esperado string) {
-    t.Helper()
+	t.Helper()
 
-    if resultado != esperado {
-        t.Errorf("resultado '%s', esperado '%s', dado '%s'", resultado, esperado, "teste")
-    }
+	if resultado != esperado {
+		t.Errorf("resultado '%s', esperado '%s', dado '%s'", resultado, esperado, "teste")
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("palavra existente", func(t *testing.T) {
+        palavra := "teste"
+        definicao := "isso é apenas um teste"
+        novaDefinicao := "nova definição"
+        dicionario := Dicionario{palavra: definicao}
+        err := dicionario.Atualiza(palavra, novaDefinicao)
+
+        comparaErro(t, err, nil)
+        comparaDefinicao(t, dicionario, palavra, novaDefinicao)
+    })
+
+    t.Run("palavra nova", func(t *testing.T) {
+        palavra := "teste"
+        definicao := "isso é apenas um teste"
+        dicionario := Dicionario{}
+
+        err := dicionario.Atualiza(palavra, definicao)
+
+        comparaErro(t, err, ErrPalavraInexistente)
+    })
 }
